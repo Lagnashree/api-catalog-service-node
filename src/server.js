1d192ba8403cc7e59cfb14dbd15cbeccd5879c45
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2022-11-10 22:50:07 +0100

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({ path: path.join(__dirname, '../') + '.env' });
+dotenv.load();
+
+app.use(express.json());
+
+app.use('/api/v1/catalog-service/api', require('./controller/crudController'));
+
+app.use((req, res) => {
+    console.log('in 404')
+    res.status(404).send('No such API');
+});
+app.use((err, req, res) => {
+    console.error(err);
+    res.status(500).send(err.response || 'Something broke!');
+});
+const server = app.listen(8080, function () {
+    const port = server.address().port;
+    console.log("Microservice is running at port:" + port);
+});
