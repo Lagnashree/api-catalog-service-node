a8d0daecfc3414c6bbd9cb90964ad8a5457b5546
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2022-11-14 21:55:55 +0100

    Update gcsRepository.js

--- src/repository/gcsRepository.js
+++ src/repository/gcsRepository.js
@@ -1,21 +1,31 @@
 const path = require("path");
 const { Storage } = require("@google-cloud/storage");
 
-
-
-exports.postFile = async function (apiName, apiVersion, environment, fileContent) {
-    let serviceKey = "";
+let serviceKey = "";
     const projectId = 'testpoc-361819';
     let bucketName = "api_catalog_bucket";
     serviceKey = path.join(__dirname, "..", "/service-account.json");
     const storage = new Storage({ projectId: projectId, keyFilename: serviceKey });
+
+async function postFile (apiName, apiVersion, environment, fileContent) {
     let fileName = apiName + "_" + apiVersion + "_" + environment + ".yml";
     await storage
         .bucket(bucketName)
         .file(fileName)
         .save(fileContent)
     console.log("file successfully uploaded to gcs bucket");
+} 
+
+async function deleteFile (apiName, apiVersion, environment){
+    let fileName = apiName + "_" + apiVersion + "_" + environment + ".yml";
+    await storage
+        .bucket(bucketName)
+        .delete(fileName)
+    console.log("file successfully deleted from gcs bucket");
+
 }
 
-
-
+module.exports = {
+    postFile,
+    deleteFile
+}
