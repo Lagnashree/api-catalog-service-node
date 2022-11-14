fa3b531f80e2a050cb406e6096216e4ea09a10c1
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2022-11-14 21:55:27 +0100

    Update crudController.js

--- src/controller/crudController.js
+++ src/controller/crudController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const postApi = require('../service/createApiService');
 const StatusCodes = require('http-status-codes');
+const { BaseError, notFound, internalServerError, serviceUnavailable, badRequest} = require('../utils/error')
 
 router.post('/', async (req, res) => {
     console.log("post");
@@ -12,21 +13,19 @@ router.post('/', async (req, res) => {
         console.log('req body', req.body);
         if (typeof req.body.apiVersion == 'undefined' || req.body.apiVersion == '' || typeof req.body.environment == 'undefined' || req.body.environment == '' || typeof req.body.apiName == 'undefined' || req.body.apiName == '' || typeof req.body.catalogName == 'undefined' || req.body.catalogName == '' || typeof req.body.apiSecurity == 'undefined' || req.body.apiSecurity == '' || typeof req.body.specUrl == 'undefined' || req.body.specUrl == '') {
             //statusCode = StatusCodes.BAD_REQUEST;
-            reasonPhrase = 'BAD_REQUEST';
-            statusCode = 400;
-            throw new Error("apiVersion, environment, apiName, catalogName, apiSecurity and specUrl are a manadatory field");
+            throw new BaseError(badRequest, "apiVersion, environment, apiName, catalogName, apiSecurity and specUrl are a manadatory field");
         }
         else if (req.body.environment && !['dev', 'stage', 'prod'].includes(req.body.environment.toLowerCase())) {
             reasonPhrase = 'BAD_REQUEST';
             statusCode = 400;
             //statusCode = StatusCodes.BAD_REQUEST;
-            throw new Error("Invalid environment field. Value should be one of dev, stage, prod");
+            throw new BaseError(badRequest,"Invalid environment field. Value should be one of dev, stage, prod");
         }
         if (req.body.apiSecurity && !['apiKey', 'bearer'].includes(req.body.apiSecurity.toLowerCase())) {
             reasonPhrase = 'BAD_REQUEST';
             statusCode = 400;
             //statusCode = StatusCodes.BAD_REQUEST;
-            throw new Error("Invalid apiSecurity value. Value should be apiKey or bearer");
+            throw new BaseError(badRequest,"Invalid apiSecurity value. Value should be apiKey or bearer");
         }
         const postAPIResult = await postApi.postApiInvoker(req.body);
         console.log(postAPIResult);
@@ -47,6 +46,8 @@ router.post('/', async (req, res) => {
     }
     res.status(statusCode).send(responsePayload);
 })
+
+
 
 router.get('/', async (req, res) => {
     console.log("get");
@@ -77,6 +78,8 @@ router.get('/', async (req, res) => {
 
 })
 
+
+
 router.delete('/:apiName/:apiVersion/:environment', async (req, res) => {
     let statusCode = 404;
     let reasonPhrase = 'BAD_REQUEST';
@@ -84,12 +87,12 @@ router.delete('/:apiName/:apiVersion/:environment', async (req, res) => {
         if (typeof req.params.apiName == 'undefined' || req.params.apiName == '' || typeof req.params.apiVersion == 'undefined' || req.params.apiVersion == '' || typeof req.params.environment == 'undefined' || req.params.environment == '') {
             statusCode = StatusCodes.BAD_REQUEST;
             reasonPhrase = 'BAD_REQUEST';
-            throw new Error("apiName, apiVersionn and environment are manadatory field");
+            throw new BaseError(badRequest,"apiName, apiVersionn and environment are manadatory field");
         }
         else if (req.params.environment && !['prod', 'ppe', 'cte-f'].includes(req.params.environment.toLowerCase())) {
             statusCode = StatusCodes.BAD_REQUEST;
             reasonPhrase = 'BAD_REQUEST';
-            throw new Error("Invalid environment field. Value should be one of cte-f, ppe, prod");
+            throw new BaseError(badRequest,"Invalid environment field. Value should be one of cte-f, ppe, prod");
         }
         let deleteInfo = await deleteApiService.deleteApiInfo(req.params.apiName, req.params.apiVersion, req.params.environment.toUpperCase());
         if (subscriptionInfo.status === 200) {
@@ -104,6 +107,39 @@ router.delete('/:apiName/:apiVersion/:environment', async (req, res) => {
             "title": reasonPhrase,
             "status": statusCode,
             "operation": 'delete'
+        }
+    }
+    res.status(statusCode).send(responsePayload);
+})
+
+
+router.patch('/:apiName/:apiVersion/:environment', async (req, res) => {
+    let statusCode = 404;
+    let reasonPhrase = 'BAD_REQUEST';
+    try {
+        if (typeof req.params.apiName == 'undefined' || req.params.apiName == '' || typeof req.params.apiVersion == 'undefined' || req.params.apiVersion == '' || typeof req.params.environment == 'undefined' || req.params.environment == '') {
+            statusCode = StatusCodes.BAD_REQUEST;
+            reasonPhrase = 'BAD_REQUEST';
+            throw new BaseError(badRequest,"apiName, apiVersionn and environment are manadatory field");
+        }
+        else if (req.params.environment && !['prod', 'ppe', 'cte-f'].includes(req.params.environment.toLowerCase())) {
+            statusCode = StatusCodes.BAD_REQUEST;
+            reasonPhrase = 'BAD_REQUEST';
+            throw new BaseError(badRequest,"Invalid environment field. Value should be one of cte-f, ppe, prod");
+        }
+        let patchInfo = await patchApiService.patchApiInfo(req.params.apiName, req.params.apiVersion, req.params.environment.toUpperCase(), req.body);
+        if (subscriptionInfo.status === 200) {
+            console.log('patchInfo', patchInfo.detail);
+            statusCode = patchInfo.status;
+        }
+        statusCode = StatusCodes.OK;
+    }
+    catch (error) {
+        responsePayload = {
+            "type": errorType,
+            "title": reasonPhrase,
+            "status": statusCode,
+            "operation": 'patch'
         }
     }
     res.status(statusCode).send(responsePayload);
